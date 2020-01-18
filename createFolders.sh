4b6fff4babb2d3e30f9dea7b5d5e5b0ff55d0d7e
relief-melone <christian.debiasi@hotmail.de>
2020-01-18 15:46:16 +0100

    added module alias

---
+++ createFolders.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir -p ./src/classes
+mkdir -p ./src/configs
+mkdir -p ./src/controllers
+mkdir -p ./src/middleware
+mkdir -p ./src/routes
+mkdir -p ./src/schemas
+mkdir -p ./src/services
+mkdir -p ./src/types
