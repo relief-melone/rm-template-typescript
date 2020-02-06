c56d0907081ea8278831e0522a7246cd505a4546
de Biasi Christian <christian.de-biasi@bmw.de>
2020-02-06 15:46:20 +0100

    space after prompts

--- scripts/setRemoteUrl.sh
+++ scripts/setRemoteUrl.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-read -p "Enter new Remote Repo Url:" REPO_URL
+read -p "Enter new Remote Repo Url:   " REPO_URL
 
 git remote set-url origin $REPO_URL
 
-read -p "Push to master and create new init Branch? (Y/n)" $PUSH_TO_MASTER
+read -p "Push to master and create new init Branch? (Y/n)  " $PUSH_TO_MASTER
 
 if [[ $PUSH_TO_MASTER != "n" ]]; then
   git add .
