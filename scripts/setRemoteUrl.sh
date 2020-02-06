09c3fe5180817cca872ac42b2cbfca5dbd6026ce
de Biasi Christian <christian.de-biasi@bmw.de>
2020-02-06 16:42:04 +0100

    init as new git repo

--- scripts/setRemoteUrl.sh
+++ scripts/setRemoteUrl.sh
@@ -2,7 +2,9 @@
 
 read -p "Enter new Remote Repo Url:   " REPO_URL
 
-git remote set-url origin $REPO_URL
+rm -rf ./.git
+git init
+git remote add origin $REPO_URL
 
 read -p "Push to master and create new init Branch? (Y/n)  " $PUSH_TO_MASTER
 
